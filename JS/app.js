b549949ffa08d270ef51b22be0f44d53c965dcf0
Ildi Dvorani <dvoranii@ildidev.com>
2022-11-14 21:51:41 -0500

    added navbar

--- JS/app.js
+++ JS/app.js
@@ -75,3 +75,22 @@ submitBtn.addEventListener("click", (e) => {
     });
   });
 });
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav__link");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+
+  console.log("clicked");
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+    }
+  });
+  // Burger animation
+  burger.classList.toggle("toggle");
+});
