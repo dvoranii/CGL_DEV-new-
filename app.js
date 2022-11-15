59a7f149681a7aaee6fc9acbeb8986d1b4d88c58
Ildi Dvorani <dvoranii@ildidev.com>
2022-11-15 11:54:23 -0500

    changed folder structures

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-import ref from "./test.js";
-import { addDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+
+// import API_KEY from "./apikey";
+
+const firebaseConfig = {
+  apiKey: " AIzaSyDaN8wrzCDOfyhNBevzyb1nX8g25WT_V2c",
+  authDomain: "cgl-forms.firebaseapp.com",
+  databaseURL: "https://cgl-forms-default-rtdb.firebaseio.com",
+  projectId: "cgl-forms",
+  storageBucket: "cgl-forms.appspot.com",
+  messagingSenderId: "1008506608692",
+  appId: "1:1008506608692:web:47818afefcc2935608be61",
+};
+
+const app = initializeApp(firebaseConfig);
+let db = getFirestore(app);
+
+var ref = collection(db, "quotes");
 
 async function addDocument_AutoID(inputs) {
   //   var ref = collection(db, "quotes");
@@ -122,3 +143,5 @@ burger.addEventListener("click", () => {
   // Burger animation
   burger.classList.toggle("toggle");
 });
+
+console.log("seomting");
